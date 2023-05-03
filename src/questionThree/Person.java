a3db25ea7d8d984ca802bca46c01ff9c2c52f1e8
rasooli <mahdierasooli693@gmail.com>
2023-05-03 21:42:55 +0430

    making the person class

---
+++ src/questionThree/Person.java
@@ -0,0 +1,42 @@
+package questionThree;
+
+import java.security.PrivateKey;
+
+public class Person {
+    private Address address;
+    private String firstName;
+    private String LastName;
+    private int age;
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
