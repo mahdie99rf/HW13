61347a3994ccb555b0114e808ed08b6d7343fb9a
rasooli <mahdierasooli693@gmail.com>
2023-05-03 21:42:36 +0430

    making the Address class

---
+++ src/questionThree/Address.java
@@ -0,0 +1,40 @@
+package questionThree;
+
+public class Address {
+    private String postalCode;
+    private String street;
+    private String zone;
+    private String city;
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getZone() {
+        return zone;
+    }
+
+    public void setZone(String zone) {
+        this.zone = zone;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
