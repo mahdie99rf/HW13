c678272ec23e28c4b2e750cccdf6c6eff50cb8bc
rasooli <mahdierasooli693@gmail.com>
2023-05-04 00:01:28 +0430

    solving Question one

---
+++ src/questionOne/LambdaUtil.java
@@ -0,0 +1,187 @@
+package questionOne;
+
+import java.math.BigDecimal;
+import java.util.Map;
+import java.util.Random;
+import java.util.function.*;
+
+public class LambdaUtil {
+
+    /**
+     * Returns {@link Supplier} that always supply "Hello"
+     *
+     * @return a string supplier
+     */
+    public static Supplier<String> helloSupplier() {
+        Supplier<String> helloSupplier = () -> "Hello";
+        System.out.println(helloSupplier.get());
+        return helloSupplier;
+    }
+
+    /**
+     * Returns a {@link Predicate} of string that checks if string is empty
+     *
+     * @return a string predicate
+     */
+    public static Predicate<String> isEmptyPredicate() {
+        Predicate<String> isEmpty = s -> s == null || s.trim().isEmpty();
+        return isEmpty;
+    }
+
+    /**
+     * Return a {@link Function} that accepts {@link String} and returns that string repeated n time, where n is passed
+     * as function argument
+     *
+     * @return function that repeats Strings
+     */
+    public static BiFunction<String, Integer, String> stringMultiplier() {
+//        Function<String, Function<Integer, String>> stringMultiplier = str -> n -> str.repeat(n);
+        //search I couldn't find repeat method
+        return null;
+    }
+
+    /**
+     * Returns a {@link Function} that converts a {@link BigDecimal} number into a {@link String} that start with
+     * a dollar sign and then gets a value
+     *
+     * @return function that converts adds dollar sign
+     */
+    public static Function<BigDecimal, String> toDollarStringFunction() {
+        Function<BigDecimal, String> addDollarsign = number -> "$" + number.toString();
+        return null;
+    }
+
+    /**
+     * Receives two parameter that represent a range and returns a {@link Predicate<String>} that verifies if string
+     * length is in the specified range. E.g. min <= length < max
+     *
+     * @param min min length
+     * @param max max length
+     * @return a string predicate
+     */
+    public static Predicate<String> lengthInRangePredicate(int min, int max) {
+        int minlength = 1;
+        int maxlength = 10;
+        Predicate<String> lengthInRangePredicate = s -> s.length() >= minlength && s.length() < maxlength;
+        return null;
+    }
+
+    /**
+     * Returns a {@link Supplier} of random integers
+     *
+     * @return int supplier
+     */
+    public static IntSupplier randomIntSupplier() {
+        Supplier<Integer> randomIntSupplier = () -> new Random().nextInt();
+        int num1 = randomIntSupplier.get();
+        int num2 = randomIntSupplier.get();
+        return null;
+    }
+
+
+    /**
+     * Returns an {@link IntUnaryOperator} that receives an int as a bound parameter, and returns a random int
+     *
+     * @return int operation
+     */
+    public static IntUnaryOperator boundedRandomIntSupplier() {
+        //todo
+        return null;
+    }
+
+    /**
+     * Returns {@link IntUnaryOperator} that calculates an integer square
+     *
+     * @return square operation
+     */
+    public static IntUnaryOperator intSquareOperation() {
+        IntUnaryOperator intSquareOperation = x -> x * x;
+        int num = 2;
+        int intSquareNumber = intSquareOperation.applyAsInt(num);
+        return null;
+    }
+
+    /**
+     * Returns a {@link LongBinaryOperator} sum operation.
+     *
+     * @return binary sum operation
+     */
+    public static LongBinaryOperator longSumOperation() {
+        LongBinaryOperator longSumOperation = (x, y) -> x + y;
+        long num1 = 5;
+        long num2 = 15;
+        long sum = longSumOperation.applyAsLong(num1, num2);
+        return null;
+    }
+
+    /**
+     * Returns a {@link ToIntFunction<String>} that converts string to integer.
+     *
+     * @return string to int converter
+     */
+    public static ToIntFunction<String> stringToIntConverter() {
+        ToIntFunction<String> stringToIntConverter = Integer::parseInt;
+        String num = "789456123";
+        int number = stringToIntConverter.applyAsInt(num);
+        return null;
+    }
+
+    /**
+     * Receives int parameter n, and returns a {@link Supplier} that supplies {@link IntUnaryOperator}
+     * that is a function f(x) = n * x
+     *
+     * @param n a multiplier
+     * @return a function supplier
+     */
+    public static Supplier<IntUnaryOperator> nMultiplyFunctionSupplier(int n) {
+        //todo
+        return null;
+    }
+
+    /**
+     * Returns {@link Supplier} of {@link Supplier} of {@link Supplier} of {@link String} "WELL DONE".
+     *
+     * @return a supplier instance
+     */
+    public static Supplier<Supplier<Supplier<String>>> trickyWellDoneSupplier() {
+        Supplier<Supplier<Supplier<String>>> trickyWellDoneSupplier = () -> () -> () -> "WellDone";
+        String output = trickyWellDoneSupplier.get().get().get();
+        return null;
+    }
+
+    /**
+     * Returns a {@link UnaryOperator} that accepts str to str function and returns the same function composed with trim
+     *
+     * @return function that composes functions with trim() function
+     */
+    public static UnaryOperator<Function<String, String>> composeWithTrimFunction() {
+        //todo
+        return null;
+    }
+
+    /**
+     * Returns a {@link BiFunction} that has two parameters. First is {@link IntUnaryOperator} which is some integer function.
+     * Second is {@link IntPredicate} which is some integer condition. And the third is {@link IntUnaryOperator} which is
+     * a new composed function that uses provided predicate (second parameter of binary function) to verify its input
+     * parameter. If predicate returns {@code true} it applies a provided integer function
+     * (first parameter of binary function) and returns a result value, otherwise it returns an element itself.
+     *
+     * @return a binary function that receiver predicate and function and compose them to create a new function
+     */
+    public static BiFunction<IntUnaryOperator, IntPredicate, IntUnaryOperator> functionToConditionalFunction() {
+        //todo :: extra points
+        return null;
+    }
+
+    /**
+     * Returns a {@link BiFunction} which first parameter is a {@link Map} where key is a function name, and value is some
+     * {@link IntUnaryOperator}, and second parameter is a {@link String} which is a function name. If the map contains a
+     * function by a given name then it is returned by high order function otherwise an identity() is returned.
+     *
+     * @return a high-order function that fetches a function from a function map by a given name or returns identity()
+     */
+    public static BiFunction<Map<String, IntUnaryOperator>, String, IntUnaryOperator> functionLoader() {
+        //todo :: extra points
+        return null;
+    }
+}
